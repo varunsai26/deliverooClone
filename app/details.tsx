07bc5fa18b9fa36a45f53d0696d51f9e5a1aa9aa
varunsai26 <chepurivarun10@gmail.com>
2024-05-20 21:50:15 +0530

    dfs

--- app/details.tsx
+++ app/details.tsx
@@ -1,10 +1,59 @@
-import { View, Text } from "react-native";
-import React from "react";
+import { View, Text, Image, StyleSheet, TouchableOpacity } from "react-native";
+import React, { useLayoutEffect } from "react";
 import ParallaxScrollView from "@/components/ParallaxScrollView";
+import { restaurant } from "@/assets/data/restaurant";
+import Colors from "@/constants/Colors";
+import { useNavigation } from "expo-router";
+import { Ionicons } from "@expo/vector-icons";
 const details = () => {
+  const navigation = useNavigation();
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerTransparent: true,
+      headerTitle: "",
+      headerTintColor: Colors.primary,
+      headerLeft: () => {
+        <TouchableOpacity
+          onPress={() => navigation.goBack()}
+          style={styles.roundButton}
+        >
+          <Ionicons
+            name="share-outline"
+            size={20}
+            color={Colors.primary}
+            style={{ marginLeft: 16 }}
+          />
+        </TouchableOpacity>;
+      },
+      headerRight: () => {
+        <View style={styles.bar}>
+          <TouchableOpacity
+            onPress={() => navigation.goBack()}
+            style={styles.roundButton}
+          >
+            <Ionicons
+              name="search-outline"
+              size={20}
+              color={Colors.primary}
+              style={{ marginLeft: 16 }}
+            />
+          </TouchableOpacity>
+        </View>;
+      },
+    });
+  }, []);
   return (
     <>
-      <ParallaxScrollView backgroundColor={"red"} parallaxHeaderHeight={300} style={{ flex: 1 }}>
+      <ParallaxScrollView
+        backgroundColor={"#fff"}
+        parallaxHeaderHeight={300}
+        style={{ flex: 1 }}
+        renderStickyHeader={() => {
+          <View key="sticky-header" style={styles.stickySection}></View>;
+        }}
+        renderBackground={() => <Image source={restaurant.img} />}
+      >
         <View style={{ height: 100 }}>
           <Text>DETAILS</Text>
         </View>
@@ -12,5 +61,26 @@ const details = () => {
     </>
   );
 };
-
+const styles = StyleSheet.create({
+  detailsContainer: {
+    backgroundColor: Colors.lightGrey,
+  },
+  stickySection: {
+    backgroundColor: "red",
+  },
+  roundButton: {
+    width: 40,
+    height: 40,
+    borderRadius: 20,
+    backgroundColor: "#fff",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  bar: {
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    gap: 10,
+  },
+});
 export default details;
